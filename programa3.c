9e7007c08813788c618023e48d8a1c0b0f3b36de
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-10-25 14:47:30 -0600

    programa3.c

---
+++ programa3.c
@@ -0,0 +1,41 @@
+  /*Edgar Eduardo Lorenzo Cruz
+practica 11 ejercicio examen*/
+
+#include <stadio.h>
+#define  apar= 10
+#define = 100
+
+void int_matrix (int arr[apar][comp])
+{
+  int i,j;
+for(1 = 0; i < apar ; i++
+  for (j = 0; j <  comp; j++)
+{
+arr[i][j] = 0;
+ }
+}
+void llenar (arr[10][100])
+  {
+inti,j, unm, num2
+  for (i=0, i<10; i++)
+{
+  printf("Dame la cantidad")
+    scanf("%d, & num);
+    for(j=0, j<num, j++)
+    {
+    printf("Dame el numer0 de componente: (0-99)")
+    scanf("%d", &num2),
+    arr[i][num2]=1
+    }
+   }
+  }
+
+void mensual (apar[10])
+{
+  int i;
+  for (i=0, i<10, i++)
+{
+    printf("Numero de aparatos mensuales de %d: ")
+    scanf("%d" &apar_men [i]);
+    }
+    }
