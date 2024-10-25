9aa479dd4b8279a558706bd038ab6099c7007e75
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-10-25 13:31:35 -0600

    Create programa1.c

---
+++ programa1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+// Prototipo o firma de las funciones del programa
+void imprimir(char[]);
+// Definición o implementación de la función main
+int main (){
+char nombre[] = "Facultad de Ingenieria";
+imprimir(nombre);
+}
+// Implementación de las funciones del programa
+void imprimir(char s[]){
+int tam;
+for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+printf("%c", s[tam]);
+printf("\n");
+}
